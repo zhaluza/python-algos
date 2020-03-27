db123bb5b9e43e9f50487d43792b432e28982950
Zac H <zac.haluza@gmail.com>
2020-03-26 22:52:43 -0400

    refined brute force solution

--- codecademy-algos/pythagorean-triplets.py
+++ codecademy-algos/pythagorean-triplets.py
@@ -1,12 +1,17 @@
-# Given a sorted list of numbers, determine whether three numbers in the list can form a Pythagorean triplet (the square of one number equals the sum of two other squared numbers)
+# Given a list of numbers, determine whether three numbers in the list can form a Pythagorean triplet (the square of one number equals the sum of two other squared numbers)
+
+# brute force solution
+
+
 def pythTrip(list):
+    # sort the list (so that all elements are in ascending order)
     # nest three loops, like a true level 10 engineer
     # Use each loop to place a pointer
     # square each number
     # add the two smallest together and see if they equal the larger square
     # If so, return true
     # Return false at the end
-
+    list.sort()
     for i in range(len(list)):
         for j in range(i+1, len(list)):
             for k in range(i+2, len(list)):
@@ -17,4 +22,4 @@ def pythTrip(list):
 
 print(pythTrip([3, 4, 5]))  # true
 print(pythTrip([4]))  # false
-print(pythTrip([2, 3, 4, 5]))  # true
+print(pythTrip([5, 4, 3, 2]))  # true
